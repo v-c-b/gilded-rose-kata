dd77a7dab2c64566fd502b03a684de492f037db2
Volker Bergner <volker@bergner.one>
2019-10-22 13:20:13 +0100

    simplified and removed unneccessary if statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,39 +7,31 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros"
-        if item.quality > 0
-            item.quality = item.quality - 1
-        end
+        item.quality = [item.quality-1, 0].max
       else
+        # item.quality = [item.quality+1, 50].min
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11 and item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
             end
             if item.sell_in < 6 and item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
             end
           end
         end
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
+        item.sell_in -= 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = 0
-          end
+        item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.name != "Aged Brie" and item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = [item.quality-1, 0].max
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
