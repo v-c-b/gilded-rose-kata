873e2065285ba9aa22ae2d270123cb6ec2985850
Volker Bergner <volker@bergner.one>
2019-10-22 17:05:58 +0100

    Simplified if not else statements to if equals else statements

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,9 +6,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros"
-        item.quality = [item.quality-1, 0].max
-      else
+      if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert" # and item.name != "Sulfuras, Hand of Ragnaros"
           item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
@@ -18,20 +16,23 @@ class GildedRose
                 item.quality += 1
             end
           end
-        item.quality = [item.quality, 50].min
-        item.quality = 80 if item.name == "Sulfuras, Hand of Ragnaros"
+      else
+        item.quality = [item.quality-1, 0].max
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      item.quality = [item.quality, 50].min
+      if item.name == "Sulfuras, Hand of Ragnaros"
+        item.quality = 80
+      else
         item.sell_in -= 1
       end
       if item.sell_in < 0
         item.quality = 0 if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        if item.name != "Aged Brie" and item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = [item.quality-1, 0].max
-        else
+        if item.name == "Aged Brie" or item.name == "Sulfuras, Hand of Ragnaros"
           if item.quality < 50
             item.quality += 1
           end
+        else
+        item.quality = [item.quality-1, 0].max
         end
       end
     end
