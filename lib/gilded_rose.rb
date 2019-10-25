e8dd69b03d9025c2d691dc38050f585577314bc1
Volker Bergner <volker@bergner.one>
2019-10-25 15:07:11 +0100

    Extracted routing to different update types into separate method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,44 +1,46 @@
 class GildedRose
   def initialize(items)
     @items = items
-    @special_items = [
-      'Backstage passes to a TAFKAL80ETC concert',
-      'Aged Brie',
-      'Sulfuras, Hand of Ragnaros',
-      'Conjured'
-    ]
+    @staggered_behaviour_items = ['Backstage passes to a TAFKAL80ETC concert']
+    @always_increase_items = ['Aged Brie']
+    @accelerated_decay_items = ['Conjured Mana Cake']
+    @legendary_items = ['Sulfuras, Hand of Ragnaros']
   end
 
   def update_quality
     @items.each do |item|
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        case item.sell_in
-        when 6..10
-          item.quality = increase_by_but_not_over(item.quality, 2, 50)
-        when 1..5
-          item.quality = increase_by_but_not_over(item.quality, 3, 50)
-        when -Float::INFINITY..0
-          item.quality = 0
-        else
-          item.quality = increase_by_but_not_over(item.quality, 1, 50)
-        end
+      case route(item.name)
+      when 'staggered'
+        item.quality = update_staggered(item: item)
+      when 'always_increase'
+        item.quality = update_general(item: item, change_pos_sell_in: 1, change_neg_sell_in: 2)
+      when 'accelerated_decay'
+        item.quality = update_general(item: item, change_pos_sell_in: -2, change_neg_sell_in: -4)
+      when 'legendary'
+        item.sell_in += 1
+      else
+        item.quality = update_general(item: item, change_pos_sell_in: -1, change_neg_sell_in: -2)
       end
-      if item.name.include?('Conjured')
-        item.quality = update_general_type(item: item, change_pos_sell_in: -2, change_neg_sell_in: -4 )
-      end
-      if item.name == 'Aged Brie'
-        item.quality = update_general_type(item: item, change_pos_sell_in: 1, change_neg_sell_in: 2 )
-      end
-      unless @special_items.include?(item.name) or item.name.include?('Conjured')
-        item.quality = update_general_type(item: item, change_pos_sell_in: -1, change_neg_sell_in: -2 )
-      end
-      item.sell_in -= 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+      item.sell_in -= 1
     end
   end
 
   private
 
-  def update_general_type(item:, change_pos_sell_in:, change_neg_sell_in:)
+  def update_staggered(item:)
+    case item.sell_in
+    when 6..10
+      item.quality = [[item.quality + 2, 0].max, 50].min
+    when 1..5
+      item.quality = [[item.quality + 3, 0].max, 50].min
+    when -Float::INFINITY..0
+      item.quality = 0
+    else
+      item.quality = [[item.quality + 1, 0].max, 50].min
+    end
+  end
+
+  def update_general(item:, change_pos_sell_in:, change_neg_sell_in:)
     case item.sell_in
     when 1..Float::INFINITY
       [[item.quality + change_pos_sell_in, 0].max, 50].min
@@ -47,12 +49,11 @@ class GildedRose
     end
   end
 
-  def increase_by_but_not_over(item, increment, limit)
-    [item + increment, limit].min
-  end
-
-  def decrease_by_but_not_below(item, decrement, limit)
-    [item - decrement, limit].max
+  def route(item_name)
+    return 'staggered' if @staggered_behaviour_items.include?(item_name)
+    return 'always_increase' if @always_increase_items.include?(item_name)
+    return 'accelerated_decay' if @accelerated_decay_items.include?(item_name)
+    return 'legendary' if @legendary_items.include?(item_name)
   end
 end
 
